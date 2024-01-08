4d7965364aee2cff19d3145be64df436f1a3a95b
King <wangya1907@163.com>
2024-01-08 16:48:04 +0800

    middlewares

---
+++ sweibo/middlewares.py
@@ -0,0 +1,186 @@
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+from scrapy import signals
+import scrapy
+import time
+from jinja2 import Template
+from playwright.sync_api import sync_playwright
+from scrapy.utils.project import get_project_settings
+import cookie
+import textwrap
+
+
+class SweiboSpiderMiddleware:
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the spider middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_spider_input(self, response, spider):
+        # Called for each response that goes through the spider
+        # middleware and into the spider.
+
+        # Should return None or raise an exception.
+        return None
+
+    def process_spider_output(self, response, result, spider):
+        # Called with the results returned from the Spider, after
+        # it has processed the response.
+
+        # Must return an iterable of Request, or item objects.
+        for i in result:
+            yield i
+
+    def process_spider_exception(self, response, exception, spider):
+        # Called when a spider or process_spider_input() method
+        # (from other spider middleware) raises an exception.
+
+        # Should return either None or an iterable of Request or item objects.
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        # Called with the start requests of the spider, and works
+        # similarly to the process_spider_output() method, except
+        # that it doesn’t have a response associated.
+
+        # Must return only requests (not items).
+        for r in start_requests:
+            yield r
+
+    def spider_opened(self, spider):
+        spider.logger.info("Spider opened: %s" % spider.name)
+
+
+class SweiboDownloaderMiddleware:
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the downloader middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_request(self, request, spider):
+        # Called for each request that goes through the downloader
+        # middleware.
+
+        # Must either:
+        # - return None: continue processing this request
+        # - or return a Response object
+        # - or return a Request object
+        # - or raise IgnoreRequest: process_exception() methods of
+        #   installed downloader middleware will be called
+        return None
+
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+        return response
+
+    def process_exception(self, request, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info("Spider opened: %s" % spider.name)
+
+
+class PlaywrightMiddleware:
+    def __init__(self):
+        self.cookies = cookie.cookies
+        self.headers = cookie.headers
+        self.last_login_time = cookie.last_time
+
+    def process_request(self, request, spider):
+        if not self.cookies or not self.headers or self.should_relogin():
+            self.login_and_get_cookies()
+
+        request.cookies = self.cookies
+        request.headers.update(self.headers)
+
+    def should_relogin(self):
+        settings = get_project_settings()
+        relogin_interval = settings.get('RELOGIN_INTERVAL', 3600*8)  # 默认为1小时
+        current_time = time.time()
+        if self.last_login_time is None or current_time - self.last_login_time > relogin_interval:
+            return True
+        return False
+
+    def login_and_get_cookies(self):
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch(headless=False)
+            context = browser.new_context()
+            page = context.new_page()
+
+            # 打开微博登录页面
+            page.goto('https://weibo.com/login.php')
+
+            # 输入用户名和密码
+            for i in range(3):
+                page.wait_for_timeout(1000)
+                page.fill('#loginname', input())
+                page.wait_for_timeout(500)
+                page.fill('[type="password"]', input())
+                page.fill('#loginname', input())
+                # 点击登录按钮
+                page.wait_for_timeout(500)
+                page.click('[node-type="submitBtn"]')
+                page.fill('[value="验证码"]', input())
+                page.click('[node-type="submitBtn"]')
+                # 等待登录成功
+                try:
+                    page.wait_for_selector('[title=全部关注]')
+                    break
+                except Exception:
+                    continue
+
+            # 获取cookies和headers信息
+            self.cookies = {cookie['name']: cookie['value'] for cookie in context.cookies()}
+            self.headers = {
+                'User-Agent': page.evaluate('navigator.userAgent')
+            }
+            # 更新最后登录时间
+            self.last_login_time = time.time()
+            f = open('./cookie.py', 'w', encoding='utf-8')
+            template = Template(textwrap.dedent("""cookies = {{ cookies }}
+            headers = {{ headers }}
+            last_time = {{ last_time }}
+            
+            """))
+            f.write(template.render(cookies=self.cookies, headers=self.headers, last_time=self.last_login_time))
+            f.close()
+            # 关闭浏览器
+            browser.close()
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        middleware = cls()
+        crawler.signals.connect(middleware.spider_closed, signal=scrapy.signals.spider_closed)
+        return middleware
+
+    def spider_closed(self, spider):
+        self.cookies = None
+        self.headers = None
+        self.last_login_time = None
---
+++ sweibo/spiders/weibo_top.py
@@ -0,0 +1,113 @@
+# -*- encoding: utf-8 -*-
+"""
+@File   : weibo_top.py
+@Time   : 2024/1/2 21:51
+@Author : king
+@Project : pytest_fortest
+"""
+
+import scrapy
+from ..items import sweiboItem
+
+
+class WeiboTopSpider(scrapy.Spider):
+    name = 'weibo_top'
+    domain = 'https://s.weibo.com'
+    start_urls = ['https://s.weibo.com/top/summary?cate=entrank']
+    # cates = ['realtimehot']
+    cates = ['realtimehot', 'socialevent', 'entrank', 'sport', 'game']
+    cookies = {
+        '_s_tentry': 'passport.weibo.com',
+        'Apache': '2160923397666.412.1703745674190',
+        'SINAGLOBAL': '2160923397666.412.1703745674190',
+        'ULV': '1703745674195:1:1:1:2160923397666.412.1703745674190:',
+        'XSRF-TOKEN': 'VDx1JqiXSrimJyCp_7ITkzT1',
+        'UOR': ',,www.baidu.com',
+        'login_sid_t': '4734bafd6583dc3113dfe7a45528490f',
+        'cross_origin_proto': 'SSL',
+        'wb_view_log': '1463*9151.75',
+        'PC_TOKEN': '7d614a578b',
+        'WBStorage': '267ec170|undefined',
+        'SSOLoginState': '1704339358',
+        'SUB': '_2A25IklfODeRhGeNG7lIT8CjPwj6IHXVr7tUGrDV8PUJbkNB-LUTEkW1NS1_oSHpe5_Nyu72qLGARiqbz0DYSmTMX',
+        'SUBP': '0033WrSXqPxfM725Ws9jqgMF55529P9D9Wh2TdBo0hVOmGSr6cTSWXkQ5JpX5KzhUgL.Fo-RSK5Eehq01Kz2dJLoIEXLxKqL1hnL1K2LxKBLBonL12BLxKqL1hML1KzLxKML1-2L1hBLxK-L1KeLBKqt',
+        'WBPSESS': 'Dt2hbAUaXfkVprjyrAZT_FBdOUymDnwY3cFZT4fpsm_vFTLaVOJKfnikLryFCvr5fS0i11c5lopXALDets8iUwpKHd2Oq-v5Dy-lGvkNvESbEjIut3-yfqvn1nXYB_M5DQWCOwd1r1L6D9h_7TToxf_PUet-uJqK-4b3orw-XaUNjQsLe4VrVKs2XYWd1b3U4ieVoGBgd65R8FWbK5lPhA==',
+    }
+
+    headers = {
+        'authority': 'weibo.com',
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'client-version': 'v2.44.45',
+        # 'cookie': '_s_tentry=passport.weibo.com; Apache=2160923397666.412.1703745674190; SINAGLOBAL=2160923397666.412.1703745674190; ULV=1703745674195:1:1:1:2160923397666.412.1703745674190:; XSRF-TOKEN=VDx1JqiXSrimJyCp_7ITkzT1; UOR=,,www.baidu.com; login_sid_t=4734bafd6583dc3113dfe7a45528490f; cross_origin_proto=SSL; wb_view_log=1463*9151.75; PC_TOKEN=7d614a578b; WBStorage=267ec170|undefined; SSOLoginState=1704339358; SUB=_2A25IklfODeRhGeNG7lIT8CjPwj6IHXVr7tUGrDV8PUJbkNB-LUTEkW1NS1_oSHpe5_Nyu72qLGARiqbz0DYSmTMX; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9Wh2TdBo0hVOmGSr6cTSWXkQ5JpX5KzhUgL.Fo-RSK5Eehq01Kz2dJLoIEXLxKqL1hnL1K2LxKBLBonL12BLxKqL1hML1KzLxKML1-2L1hBLxK-L1KeLBKqt; WBPSESS=Dt2hbAUaXfkVprjyrAZT_FBdOUymDnwY3cFZT4fpsm_vFTLaVOJKfnikLryFCvr5fS0i11c5lopXALDets8iUwpKHd2Oq-v5Dy-lGvkNvESbEjIut3-yfqvn1nXYB_M5DQWCOwd1r1L6D9h_7TToxf_PUet-uJqK-4b3orw-XaUNjQsLe4VrVKs2XYWd1b3U4ieVoGBgd65R8FWbK5lPhA==',
+        'referer': 'https://weibo.com/',
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'server-version': 'v2024.01.03.1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
+        'x-xsrf-token': 'VDx1JqiXSrimJyCp_7ITkzT1',
+    }
+
+    def start_requests(self):
+        for cate in self.cates:
+            item = sweiboItem()
+            item['module'] = cate
+            url = f'https://s.weibo.com/top/summary?cate={cate}'
+            yield scrapy.Request(url, meta={'item': item})
+
+    def parse(self, response):
+        hot_lists = response.css('.td-02') or response.xpath('//*[@class="td-02"]')
+        item = response.meta['item']
+        for hot in hot_lists:
+            new_item = sweiboItem()
+            new_item['module'] = item['module']
+            new_item['name'] = hot.css(' a::text').get() or hot.xpath(".//a/text()").get()
+            new_item['hot'] = hot.css(' span::text').get() or hot.xpath(".//span/text()").get()
+            if new_item['hot'] and len(new_item['hot'].split()) >= 1:
+                for i in new_item['hot'].split():
+                    if i.isdigit():
+                        new_item['hot'] = int(i)
+                        break
+            href_url = hot.css(' a::attr(href)').get() or hot.xpath(".//a/@href").get()
+            if "javascript:void(0);" in href_url:
+                continue
+            hot_search_url = f"https://s.weibo.com{href_url}"
+            yield scrapy.Request(hot_search_url, callback=self.parse_hot_search, meta={'item': new_item})
+
+    def parse_hot_search(self, response):
+        item = response.meta['item']
+        news_list = response.xpath('//div[@action-type="feed_list_item"]')
+        for news in news_list:
+            new_item = sweiboItem()
+            new_item['module'] = item['module']
+            new_item['name'] = item['name']
+            new_item['hot'] = item['hot']
+            new_item['author'] = news.css(' p[node-type="feed_list_content"]::attr(nick-name)').get()
+            new_item['author'] = news.xpath('.//p[@node-type="feed_list_content"]/@nick-name').get().strip()
+            try:
+                new_item['public_time'] = news.xpath('.//div[@class="from"]/a[1]/text()').get().strip()
+            except:
+                new_item['public_time'] = news.xpath('.//div[@class="from"]/a[1]/text()').get()
+            new_item['content'] = "".join(
+                map(str.strip, news.xpath('.//p[@node-type="feed_list_content"]/text()').getall()))
+            if news.xpath('.//p[@node-type="feed_list_content_full"]/text()').getall():
+                new_item['content'] = "".join(
+                    map(str.strip, news.xpath('.//p[@node-type="feed_list_content_full"]/text()').getall()))
+            new_item['video'] = news.xpath('.//div[@aria-label="视频播放器"]/video/@src').get()
+            picture_list = news.xpath('.//div[@class="media media-piclist"]//li')
+            new_item['picture'] = []
+            if picture_list:
+                for picture in picture_list:
+                    picture_ = picture.xpath('.//img/@src').get()
+                    picture_l = picture_.split('/')
+                    picture_l[-2] = 'mw690' if 'jpg' in picture_l[-1] else picture_l[-2]
+                    picture_ = '/'.join(picture_l)
+                    new_item['picture'].append(picture_)
+                    if picture.xpath('.//video/@src').get():
+                        new_item['picture'].append(picture.xpath('.//video/@src').get())
+            yield new_item
