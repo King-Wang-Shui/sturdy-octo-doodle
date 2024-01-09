1c900f0ff0bef010fb56b4d7501bb5439ea7df52
king <wangya1907@163.com>
2024-01-09 02:39:15 +0000

    update sweibo/cookie.py.
    Signed-off-by: king <wangya1907@163.com>

--- sweibo/cookie.py
+++ sweibo/cookie.py
@@ -5,20 +5,6 @@
 # @File : cookie.py.py
 # @Project : sweibo
 
-cookies = {'tid': '01Afw4-FtKMdLpsZOIS_lusI5zF7tpXucK1EyLOzt1gV1C', 'PC_TOKEN': '6880cd3262',
-           'login_sid_t': '7d70ae6653825dc67fd6464b42a87903', 'cross_origin_proto': 'SSL',
-           'WBStorage': '267ec170|undefined', '_s_tentry': 'passport.weibo.com',
-           'Apache': '4368227239923.9165.1704707668996', 'SINAGLOBAL': '4368227239923.9165.1704707668996',
-           'ULV': '1704707668998:1:1:1:4368227239923.9165.1704707668996:', 'wb_view_log': '1280*7201.0000000447034836',
-           'tgc': 'TGT-NTg1MDIwNjM5Mg==-1704707763-tc-23624A02E6910F876F1340C56BFF0B7F-1',
-           'SUB': '_2A25In7biDeRhGeNG7lIT8CjPwj6IHXVr1LYqrDV6PUJbktAGLWf6kW1NS1_oSG6wZUNA1hPs5GBwMvXuFOH0msEM',
-           'SUBP': '0033WrSXqPxfM725Ws9jqgMF55529P9D9Wh2TdBo0hVOmGSr6cTSWXkQ5JpX5KzhUgL.Fo-RSK5Eehq01Kz2dJLoIEXLxKqL1hnL1K2LxKBLBonL12BLxKqL1hML1KzLxKML1-2L1hBLxK-L1KeLBKqt',
-           'ALC': 'ac%3D27%26bt%3D1704707763%26cv%3D6.0%26et%3D1707299763%26ic%3D1951213059%26login_time%3D1704707763%26scf%3D%26uid%3D5850206392%26vf%3D0%26vs%3D1%26vt%3D0%26es%3D5d77986303b418b424f313eea50bd714',
-           'ALF': '1707299762', 'LT': '1704707763',
-           'SRT': 'D.QqHBJZPHUZW8T!Mb4cYGS4uGi-oHPDYDTbB3TZSHNEYdVZEJdQHpMERt4EP1RckKA4WJPeHYTsVoObJoQ-yOA!PfWOBpTDHrMm9OOeSni!yGd4uh5doMU39u*B.vAflW-P9Rc0lR-ykcDvnJqiQVbiRVPBtS!r3JZPQVqbgVdWiMZ4siOzu4DbmKPWfJbM!Ac0nTESLU4WOVZSZPEiQdFkMi49ndDPI5cYPSrnlMc0k4biIi4PbTGmnJ4kwJcM1OFyHMPYJ5mkoODEfU!noNqHJ5mkCOmH6U!noJ!HJ5mkoODEfO4noN39J5mkiODmkJ!noTmHJ5mjkODEIS4oCN3Er',
-           'SRF': '1704707763', 'SSOLoginState': '1704707762', 'XSRF-TOKEN': 'EF2dIiU_TIlhe6IbUK7N2CMI',
-           'WBPSESS': 'SPBHhIMrXAGyLtvlRmW_fAdoi_51sUaHra_6D9opiS5cljho4_Yn7DbfMqmCedCvR2GTof2i3BwxtLPSGPLNkw8AJAUrHtsqdIbBaj3aPmlGFQxgdycZ1mR32Srlk9MaWeVsgfTZYnJO_O8O4mEROA=='}
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
-last_time = 1704763766.0619843
-
+cookies = {}
+headers = {}
+last_time = 0
